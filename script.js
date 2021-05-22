cc7b5910c9640025403946ad08e141d1fe5772d5
tyler <tarthurf@yahoo.com>
2021-05-21 20:39:58 -0700

    added string to array challenge

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log("Hello World!");
+
+// Write a function that will turn this string into an array where each index is one letter of the alphabet
+var alphaStr = "abcdefghijklmnopqrstuvwxyz";
+
+// alphaArr is an empty object. This is where your alphabet array should end up
+var alphaArr;
+
+const strToArr = (arr) => {
+    // write your function here
+    return console.log(alphaArr)
+}
+strToArr()
+
+// ---------------------------------------------------------------------
+
