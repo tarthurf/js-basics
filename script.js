ecd5f1a91517a9d4d144166cb419ae31f6342b2c
tyler <tarthurf@yahoo.com>
2021-05-21 20:46:49 -0700

    added calculator challenge

--- script.js
+++ script.js
@@ -6,11 +6,23 @@ var alphaStr = "abcdefghijklmnopqrstuvwxyz";
 // alphaArr is an empty object. This is where your alphabet array should end up
 var alphaArr;
 
-const strToArr = (arr) => {
+const strToArr = (str, arr) => {
     // write your function here
+
     return console.log(alphaArr)
 }
-strToArr()
+// Don't forget to add parameters for your function
+strToArr();
 
 // ---------------------------------------------------------------------
 
+// Write a function that acts as a simple calculator that takes two numbers and an operator ('+' for addition, '-' for subtraction, '*' for multiplication, '/' for division, '^' for power)
+const calculator = (num1, operator, num2) => {
+    // write your function here
+
+    // use the empty object total to hold your total number
+    var total;
+    return console.log(total)
+}
+// Don't forget to add parameters for your function
+calculator();
